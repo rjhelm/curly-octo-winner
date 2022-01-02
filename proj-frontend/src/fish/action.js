dc54a8f2ee7a8ea71640a45e5b286e5195fefb48
rjhelm <rjhelm@github.io>
2022-01-01 18:29:42 -0700

    created the CRUD operations for REST API

--- proj-frontend/src/fish/action.js
+++ proj-frontend/src/fish/action.js
@@ -7,3 +7,61 @@ import {
 
 import FishService from "./fishService";
 
+export const createFish = (name, type, age, sex,) => async (dispatch) => {
+    try {
+        const res = await FishService.create({
+            name,
+            type,
+            age,
+            sex,
+        });
+
+        dispatch({
+            type: CREATE_FISH,
+            payload: res.data,
+        });
+    } catch (err) {
+        return Promise.reject(err)
+    }
+};
+
+export const retrieveFishies = () => async (dispatch) => {
+    try {
+        const res = await FishService.getAll();
+
+        dispatch({
+            type: RETRIEVE_FISHIES,
+            payload: res.data,
+        });
+    } catch(err) {
+        console.log(err);
+    }
+};
+
+export const updateFish = (id, data) => async (dispatch) => {
+    try {
+        const res = await FishService.update(id, data);
+
+        dispatch({
+            type: UPDATE_FISH,
+            payload: data,
+        });
+        return Promise.resolve(res.data);
+    
+    } catch(err) {
+        return Promise.reject(err);
+    }
+}
+
+export const deleteFish = (id) => async (dispatch) => {
+    try {
+        await FishService.delete(id);
+        dispatch({
+            type: DELETE_FISH,
+            payload: { id },
+        });
+
+    } catch(err) {
+        console.log(err);
+    }
+};
